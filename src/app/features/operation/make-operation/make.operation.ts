282d6b7ff0a7822286feb855ee2bcd2f675f7d41
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-01-15 22:29:31 +0100

    feat: add MakeOperation component for handling user operations

---
+++ src/app/features/operation/make-operation/make.operation.ts
@@ -0,0 +1,67 @@
+import {Component, signal} from '@angular/core';
+import {OperationService} from '../../../core/services/operation.service';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-make-operation',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './make.operation.html',
+})
+export class MakeOperationComponent {
+
+  type = signal<'DEPOSIT' | 'WITHDRAW' | 'TRANSFER'>('DEPOSIT');
+  amount = 0;
+  destinationAccountId = '';
+
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  constructor(
+    private operationService: OperationService,
+    private router: Router
+  ) {
+  }
+
+  setType(type: 'DEPOSIT' | 'WITHDRAW' | 'TRANSFER') {
+    this.type.set(type);
+    this.error.set(null);
+  }
+
+  submit() {
+    this.error.set(null);
+
+    if (this.amount <= 0) {
+      this.error.set('Amount must be greater than zero');
+      return;
+    }
+
+    if (this.type() === 'TRANSFER' && !this.destinationAccountId) {
+      this.error.set('Destination account is required');
+      return;
+    }
+
+    this.loading.set(true);
+
+    const payload: any = {
+      type: this.type(),
+      amount: this.amount,
+      destinationAccountId: this.destinationAccountId || null
+    };
+
+    this.operationService.makeOperation(payload).subscribe({
+      next: () => {
+        this.loading.set(false);
+        this.router.navigate(["/operations"]).catch((error: Error) => {
+          console.log("Navigation error:", error)
+        });
+      },
+      error: err => {
+        this.loading.set(false);
+        this.error.set(err?.error?.message || 'Operation failed');
+      }
+    });
+  }
+}
