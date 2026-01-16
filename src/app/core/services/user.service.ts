aa036f3f86d61f31734aebf5a414ba49673c9f20
ayoub-ahnaou <ayoubahnaou@gmail.com>
2026-01-16 17:21:40 +0100

    style: format code for consistency in user.service.ts

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,8 +1,8 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {User} from '../models/user.model';
-import {map, Observable} from 'rxjs';
-import {ApiResponse} from '../models/api.response.model';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
+import { map, Observable } from 'rxjs';
+import { ApiResponse } from '../models/api.response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +16,27 @@ export class UserService {
       .pipe(
         map(res => res.data)
       );
+  }
+
+  // Admin Methods
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<ApiResponse<User[]>>(`${this.API_URL}/admin/users`)
+      .pipe(map(res => res.data));
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`${this.API_URL}/admin/users/${id}`);
+  }
+
+  activateUser(id: number) {
+    return this.http.patch(`${this.API_URL}/admin/users/${id}/activate`, {});
+  }
+
+  deactivateUser(id: number) {
+    return this.http.patch(`${this.API_URL}/admin/users/${id}/deactivate`, {});
+  }
+
+  promoteUser(id: number) {
+    return this.http.patch(`${this.API_URL}/admin/users/${id}/promote`, {});
   }
 }
